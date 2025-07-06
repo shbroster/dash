2c275afc320fc698e9aa41ddce39587a19e48f33
Samuel Broster <sam@broster.dev>
2025-07-06 20:36:11 +0100

    Add dog photo

--- src/components/dash.tsx
+++ src/components/dash.tsx
@@ -1,11 +1,22 @@
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "./ui/button";
-import { ListTodo, Maximize2 } from "lucide-react";
+import { Maximize2 } from "lucide-react";
 import { useState } from "react";
 import TrainsCard from "./trains/trains";
-import { cn } from "../lib/utils";
 import WeatherCard from "./weather/weather";
 import { CalendarCard } from "./calender/calendar";
+import styled, { keyframes } from "styled-components";
+
+
+const kenburns = keyframes`
+  0%   { transform: scale(0.82) translate(5%,-28%); }
+  100% { transform: scale(1.9) translate(4%, -8%); }
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: auto;
+  animation: ${kenburns} 60s ease-in-out infinite alternate;
+`;
 
 export default function HomeDashboard() {
   const [isFullscreen, setIsFullscreen] = useState(false);
@@ -51,34 +62,14 @@ export default function HomeDashboard() {
       {/* Weather Section */}
       <WeatherCard />
 
-      {/* To-Do Section */}
-      <Card className={cn("flex flex-col h-full")}>
-        <CardHeader>
-          <CardTitle className={cn("flex items-center gap-2")}>
-            <ListTodo className={cn("w-5 h-5")} /> To-Do
-          </CardTitle>
-        </CardHeader>
-        <CardContent className={cn("space-y-2")}>
-          {/*} <div className="flex items-center space-x-2">
-            <Checkbox id="task1" />
-            <label htmlFor="task1" className="text-sm">
-              Take out the trash
-            </label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <Checkbox id="task2" defaultChecked />
-            <label htmlFor="task2" className="text-sm">
-              Check train status
-            </label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <Checkbox id="task3" />
-            <label htmlFor="task3" className="text-sm">
-              Buy groceries
-            </label>
-          </div> */}
-        </CardContent>
-      </Card>
+      {/* Photo Section */}
+      <div className="relative rounded-lg overflow-hidden h-full">
+        <Img 
+          src="/src/assets/dog.jpg"
+          alt="Cute Dog"
+          className="w-full h-full object-cover scale-135 filter grayscale-70 contrast-100 brightness-125"
+        />
+      </div>
     </div>
   );
 }
